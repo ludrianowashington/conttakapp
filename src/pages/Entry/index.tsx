c6256f68a375ca0a5fa9985a8910c0ebde80e042
Ludriano <ludinho.washi@gmail.com>
2021-11-10 00:23:14 -0300

    Criando mascara para valor digitado

--- src/pages/Entry/index.tsx
+++ src/pages/Entry/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { ProgressBar } from "react-native-paper";
 
@@ -8,7 +8,7 @@ import {
   Button,
   Image,
   ScrollView,
-  SafeAreaView, 
+  SafeAreaView,
   TouchableOpacity,
   View,
   TextInput
@@ -23,19 +23,52 @@ import styles from "./styles"
 const AppStack = createStackNavigator();
 
 
+
 export default function Entry() {
   const buttons = ['Despesa', 'Receita', 'Tranferência'];
-  
-  const [selectedItem, setSelectedItem] = useState(0)
-  const [inputValue, setInputValue] = useState('')
 
-  const iconSelect = (selectedItem:number)=>{
-    if (selectedItem === 0) return <FontAwesome name='plus' size={19} color='#8175BA'/>
-    if (selectedItem === 1) return <FontAwesome name='minus' size={19} color='#8175BA'/>
-    if (selectedItem === 2) return <FontAwesome name='exchange' size={19} color='#8175BA'/>
+  const [selectedItem, setSelectedItem] = useState(0)
+  const [inputValue, setInputValue] = useState('0,00')
+
+  const iconSelect = (selectedItem: number) => {
+    if (selectedItem === 0) return <FontAwesome name='plus' size={19} color='#8175BA' />
+    if (selectedItem === 1) return <FontAwesome name='minus' size={19} color='#8175BA' />
+    if (selectedItem === 2) return <FontAwesome name='exchange' size={19} color='#8175BA' />
   }
 
-  return(
+  function formatValue(value: any) {
+    let val = value;
+
+    val = val + '';
+    val = parseInt(val.replace(/[\D]+/g, ''));
+    val = val + '';
+
+    val = val.replace(/\D/g, "") // permite digitar apenas numero
+    val = val.replace(/(\d{1})(\d{14})$/, "$1.$2") // coloca ponto antes dos ultimos digitos
+    val = val.replace(/(\d{1})(\d{11})$/, "$1.$2") // coloca ponto antes dos ultimos 11 digitos
+    val = val.replace(/(\d{1})(\d{8})$/, "$1.$2") // coloca ponto antes dos ultimos 8 digitos
+    val = val.replace(/(\d{1})(\d{5})$/, "$1.$2") // coloca ponto antes dos ultimos 5 digitos
+    val = val.replace(/(\d{1})(\d{1,2})$/, "$1,$2") // coloca virgula antes dos ultimos 2 digitos
+
+    // if (val.length < 2) {
+    //   return val.replace(/([0-9]{1})/g, "0,0$1");
+    // }
+    // if (val.length == 2) {
+    //   return val.replace(/([0-9]{2})/g, "0,$1")
+    // }
+
+    // val = val.replace(/([0-9]{2})$/g, ",$1");
+
+    // if (val.length > 6) {
+    //   return val.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+    // }
+
+    if (isNaN(val)) return val.replace("0,00");
+
+    return val;
+  }
+
+  return (
     <ScrollView>
       <View style={styles.view}>
         <ButtonGroup
@@ -47,12 +80,13 @@ export default function Entry() {
           selectedButtonStyle={styles.button}
           textStyle={styles.buttonText}
         />
+
         <View style={styles.display}>
           {iconSelect(selectedItem)}
-          <TextInput 
+          <TextInput
             style={styles.inputText}
             value={inputValue}
-            onChangeText={setInputValue}
+            onChangeText={e => setInputValue(formatValue(e))}
             placeholder='0.00'
             placeholderTextColor='#8175BA'
             keyboardType='number-pad'
